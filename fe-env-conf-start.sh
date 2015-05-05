6df8acad46092683b0785ef0a7e63cf2dd7f1f1d
Olga Lisovskaya <thebestladyfox@gmail.com>
2015-05-05 14:45:06 +0600

    conditions about vars were added

--- fe-env-conf-start.sh
+++ fe-env-conf-start.sh
@@ -9,16 +9,34 @@ if [ -z "$1" ]; then
 fi
 
 if [ ! -e fe-env-conf-config.sh ]; then
-    read -p "Укажите ваш username (например, o.lisovskaya):" username
-    read -p "Укажите ваш проект (например, realty.ngs.ru):" project
-    read -p "jiraid (например, rn):" jiraid
-    read -p "Укажите репу вашего проекта (например, ssh://git@git.rn/ngs/rn.git). Можно посмотреть на git.rn:" projectrep
-    read -p "Какой name в git'е хотите? (например, Olga Lisovskaya):" gitname
-
-    echo "Советую указать эти данные в fe-env-conf-config.sh, чтобы в следующий раз не пришлось вводить вручную."
+    echo "Требуется fe-env-conf-config.sh в текущей директории, а его почему-то нет :("
 else
     source fe-env-conf-config.sh
 fi
 
+if [ -z "$username" ]; then
+    read -p "Укажите ваш username (например, o.lisovskaya):" username
+fi
+
+if [ -z "$project" ]; then
+    read -p "Укажите ваш проект (например, realty.ngs.ru):" project
+fi
+
+if [ -z "$jiraid" ]; then
+    read -p "jiraid (например, rn):" jiraid
+fi
+
+if [ -z "$projectrep" ]; then
+    read -p "Укажите репу вашего проекта (например, ssh://git@git.rn/ngs/rn.git). Можно посмотреть на git.rn:" projectrep
+fi
+
+if [ -z "$gitname" ]; then
+    read -p "Какой name в git'е хотите? (например, Olga Lisovskaya):" gitname
+fi
+
+    # echo "Советую указать эти данные в fe-env-conf-config.sh, чтобы в следующий раз не пришлось вводить вручную."
+
 ssh root@ngs.ru.$1 "username=$username project=$project bash -s" < fe-env-conf-user.sh && \
+echo "Подключились по ssh под рутом"
 ssh $username@ngs.ru.$1 "env=$1 username=$username project=$project jiraid=$jiraid projectrep=$projectrep $gitname=gitname bash -s" < fe-env-conf-other.sh && \
+echo "Подключились по ssh под вашим пользователем"
--- fe-env-conf-user.sh
+++ fe-env-conf-user.sh
@@ -3,14 +3,17 @@
 #2 Создаем пользователя под рутом на среде
 
 echo "Создаю пользователя"
-useradd $username
-cd /home/
-mkdir $username
-chown $username:$username $username
-passwd $username
-adduser $username sudo
+useradd $username && \
+cd /home/ && \
+mkdir $username && \
+chown $username:$username $username && \
+passwd $username && \
+echo "Типа создан пароль пользователя"
+adduser $username sudo && \
 
-cd /data/projects/
-rm $project
-mkdir $project
-chown $username:$username $project
+cd /data/projects/ && \
+sudo rm $project && \
+sudo mkdir $project && \
+sudo chown $username:$username $project && \
+
+echo "Пользователь создан, папка проекта создана"
