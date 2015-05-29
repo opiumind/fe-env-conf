6788f650d1b18e1b27dab04923711cb533c9a37a
Olga Lisovskaya <thebestladyfox@gmail.com>
2015-05-29 15:20:44 +0600

    fixed the mistake in determining of project version

--- fe-env-conf-config.sh
+++ fe-env-conf-config.sh
@@ -4,13 +4,13 @@
 username=o.lisovskaya
 
 # Ваш проект (например, realty.ngs.ru):
-project=realty.ngs.ru
+project=n1.ru
 
 # jiraid (например rn):
-jiraid=rn
+jiraid=realty
 
 # Репа вашего проекта (например, ssh://git@git.rn/ngs/rn.git). Можно посмотреть на git.rn.
-projectrep=ssh://git@git.rn/ngs/rn.git
+projectrep=ssh://git@git.rn/ngs/realty.git
 
 # Какой name в git'е хотите? (например, Olga Lisovskaya):
 gitname="Olga Lisovskaya"
--- fe-env-conf-start.sh
+++ fe-env-conf-start.sh
@@ -52,7 +52,7 @@ adduser $username sudo
 WHERE
 
 echo -e '\E[37;44m'"\033[1mСейчас комрад поработает\033[0m"
-project_version=$(echo $(curl -s dev.ngs.local/projects/ | grep realty.ngs.ru -A3 | tail -n-1))
+project_version=$(echo $(curl -s dev.ngs.local/projects/ | grep $project -A3 | tail -n-1))
 echo "Насколько я понял, это последняя версия проекта: "$project_version
 
 
